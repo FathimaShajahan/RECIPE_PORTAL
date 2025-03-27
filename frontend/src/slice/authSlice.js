22b144d48ea46244202869ef2a1b2b054f0c7e14
Fathima.S <69690299+FathimaShajahan@users.noreply.github.com>
2025-03-27 14:55:55 +0530

    edited

--- frontend/src/slice/authSlice.js
+++ frontend/src/slice/authSlice.js
@@ -20,35 +20,3 @@ const authSlice = createSlice({
 export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
 
-
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const initialState = {
-//     token: localStorage.getItem("access") || null,
-//     refreshToken: localStorage.getItem("refresh") || null,
-//     user: null,
-// };
-
-// const authSlice = createSlice({
-//     name: "auth",
-//     initialState,
-//     reducers: {
-//         login: (state, action) => {
-//             state.token = action.payload.token;
-//             state.refreshToken = action.payload.refresh;
-//             state.user = action.payload.user;
-//             localStorage.setItem("access", action.payload.token);
-//             localStorage.setItem("refresh", action.payload.refresh);
-//         },
-//         logout: (state) => {
-//             state.token = null;
-//             state.refreshToken = null;
-//             state.user = null;
-//             localStorage.removeItem("access");
-//             localStorage.removeItem("refresh");
-//         },
-//     },
-// });
-
-// export const { login, logout } = authSlice.actions;
-// export default authSlice.reducer;
